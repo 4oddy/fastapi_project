0a27a2a162211aa08d4aa940488bb92785afe481
4oddy <4oddy000@mail.ru>
2023-08-29 19:02:56 +0300

    exception handlers

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-from src.modules.authentication.dependencies import \
-    get_create_access_token_use_case
-from src.modules.authentication.dependencies_stubs import \
-    get_create_access_token_use_case_stub
+from src.modules.authentication.dependencies import (
+    get_authentication_service, get_create_access_token_use_case)
+from src.modules.authentication.dependencies_stubs import (
+    get_authentication_service_stub, get_create_access_token_use_case_stub)
+from src.modules.authentication.exceptions.authentication_errors import (
+    InvalidAccessTokenError, InvalidCredentialsError)
 from src.modules.authentication.presentation.routes.router import \
     authentication_router
 from src.modules.users.dependencies import (get_create_user_use_case,
@@ -19,6 +22,8 @@ from src.modules.users.dependencies_stubs import (
     get_find_by_username_use_case_stub, get_list_users_use_case_stub,
     get_update_user_use_case_stub, get_user_repository_stub,
     get_user_unit_of_work_stub)
+from src.modules.users.exceptions.user_errors import (UserAlreadyExistsError,
+                                                      UserDoesNotExistError)
 from src.modules.users.presentation.routes.router import user_router
 
 app = FastAPI()
@@ -39,6 +44,39 @@ app.dependency_overrides[get_delete_by_id_use_case_stub] = get_delete_by_id_use_
 
 # authentication module dependencies
 app.dependency_overrides[get_create_access_token_use_case_stub] = get_create_access_token_use_case
+app.dependency_overrides[get_authentication_service_stub] = get_authentication_service
+
+
+@app.exception_handler(InvalidCredentialsError)
+def invalid_credentials_error_handler(_: ..., exc: InvalidCredentialsError):
+    return JSONResponse(
+        status_code=401,
+        content={'message': exc.message}
+    )
+
+
+@app.exception_handler(InvalidAccessTokenError)
+def invalid_access_token_error_handler(_: ..., exc: InvalidAccessTokenError):
+    return JSONResponse(
+        status_code=401,
+        content={'message': exc.message}
+    )
+
+
+@app.exception_handler(UserAlreadyExistsError)
+def user_already_exists_error_handler(_: ..., exc: UserAlreadyExistsError):
+    return JSONResponse(
+        status_code=409,
+        content={'message': exc.message}
+    )
+
+
+@app.exception_handler(UserDoesNotExistError)
+def user_does_not_exist_error_handler(_: ..., exc: UserDoesNotExistError):
+    return JSONResponse(
+        status_code=404,
+        content={'message': exc.message}
+    )
 
 
 @app.get(
--- src/modules/authentication/dependencies_stubs.py
+++ src/modules/authentication/dependencies_stubs.py
@@ -1,4 +1,9 @@
 from .application.create_access_token.use_case import CreateAccessTokenUseCase
+from .services.authentication_service import AuthenticationService
+
+
+def get_authentication_service_stub() -> AuthenticationService:
+    ...
 
 
 def get_create_access_token_use_case_stub() -> CreateAccessTokenUseCase:
